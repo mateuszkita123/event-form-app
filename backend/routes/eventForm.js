1b89fc606f41c3dea2e89732fee1feed0a46e9f5
unknown <mateuszkita123@gmail.com>
2020-02-09 18:26:40 +0100

    date picker implemented

--- backend/routes/eventForm.js
+++ backend/routes/eventForm.js
@@ -3,15 +3,30 @@ let mongoose = require('mongoose'),
   router = express.Router();
 
 // User Model
-let userSchema = require('../models/user');
+let userSchema = require('../models/User');
 
-// CREATE User
-router.route('/add-user-data').post((req, res, next) => {
-  userSchema.create(req.body, (error, data) => {
+// READ Users
+router.route('/').get((req, res) => {
+  userSchema.find((error, data) => {
     if (error) {
       return next(error)
     } else {
-      console.log(data)
+      console.log("getting all users data: ", data)
+      res.json(data)
+    }
+  })
+})
+
+// CREATE User
+router.route('/add-user-data').post((req, res, next) => {
+  console.log("req.body: ", req.body);
+  console.log("userSchema: ", userSchema);
+  userSchema.create(req.body, (error, data) => {
+    if (error) {
+      console.log("error while saving to db: ", error);
+      return next(error)
+    } else {
+      console.log("created user with data: ", data)
       res.json(data)
     }
   })
--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cors());
-app.use('/', formRoute)
+app.use('/users', formRoute);
 
 // PORT
 const port = process.env.PORT || 4000;
